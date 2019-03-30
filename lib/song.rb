82137b07b6b8e83ff8a3c3b72f8b61de0ff4e963
Zoncharne Lee <zoncharne@gmail.com>
2019-03-30 22:28:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 class Song
   attr_accessor :name
   attr_reader :artist, :genre
-  
+
   @@all = []
-  
+
   def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist = artist if artist
@@ -19,19 +19,19 @@ class Song
     @genre = genre
     genre.songs << self unless genre.songs.include?(self)
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.destroy_all
-    self.all.clear
+    all.clear
   end
-  
+
   def save
     self.class.all << self
   end
-  
+
   def self.create(name)
     song = new(name)
     song.save
@@ -46,4 +46,17 @@ class Song
     find_by_name(name) || create(name)
   end
 
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+
+    new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{ |s| s.save }
+  end
 end
