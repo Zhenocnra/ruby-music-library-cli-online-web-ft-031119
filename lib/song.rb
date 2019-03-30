5de625eef522f5606e5376392621bd724afeed07
Zoncharne Lee <zoncharne@gmail.com>
2019-03-30 22:14:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,20 @@ class Song
   
   @@all = []
   
-  def initialize(name)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
-    @@all << self
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
   end
   
   def self.all
