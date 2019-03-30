80aa73ea862a4a4c2ab102bc3d6bf62593110551
Zoncharne Lee <zoncharne@gmail.com>
2019-03-30 22:28:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,38 +1,42 @@
 class Artist
+  extend Concerns::Findable
+
   attr_accessor :name
   attr_reader :songs
-  
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
-    @@all << self
     @songs = []
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.destroy_all
-    self.all.clear
+    all.clear
   end
-  
+
   def save
-    @@all << self
+    self.class.all << self
   end
-  
+
   def self.create(name)
     artist = new(name)
     artist.save
     artist
+
+    # Or, as a one-liner:
+    # new(name).tap{ |a| a.save }
   end
-  
+
   def add_song(song)
     song.artist = self unless song.artist
-    songs << song unless song.include?(song)
+    songs << song unless songs.include?(song)
   end
-  
+
   def genres
     songs.collect{ |s| s.genre }.uniq
   end
