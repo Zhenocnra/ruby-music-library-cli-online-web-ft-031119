7248ca83ee39bd05b79f7ba39a9b8f3cb1178872
Zoncharne Lee <zoncharne@gmail.com>
2019-03-30 22:28:41 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,26 +1,29 @@
+
 class Genre
+  extend Concerns::Findable
+
   attr_accessor :name
   attr_reader :songs
 
   @@all = []
-  
+
   def initialize(name)
     @name = name
-    @@all << self
+    @songs = []
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.destroy_all
-    self.all.clear
+    all.clear
   end
-  
+
   def save
-    @@all << self
+    self.class.all << self
   end
-  
+
   def self.create(name)
     genre = new(name)
     genre.save
@@ -30,5 +33,4 @@ class Genre
   def artists
     songs.collect{ |s| s.artist }.uniq
   end
-  
 end
